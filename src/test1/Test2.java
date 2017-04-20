a0a971c543ea25029785f9e2c71fddeec9e6d5d1
Estudiante <Estudiante@10.203.2.203>
2017-04-20 15:10:33 -0500

    s

---
+++ src/test1/Test2.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package test1;
+
+/**
+ *
+ * @author Estudiante
+ */
+public class Test2 extends Exception{
+
+    public Test2(){
+        super("Error XXXX");
+    }
+    
+}
